71e48fd041538031add64aafab724c2fd93fbfae
Vlad <speaker73.mieln@gmail.com>
2018-02-20 05:02:59 +0200

    refactoring

--- components/Users/User.js
+++ components/Users/User.js
@@ -15,7 +15,7 @@ export default class User extends PureComponent {
             		});
           		}}
         >
-          <Image style={{width:100, height:100}} width={100} heigth={100} source={{uri:this.props.user.avatar_url}} />
+          <Image style={{width:100, height:100}} source={{uri:this.props.user.avatar_url}} />
           <View style={{paddingLeft:20}}>
 				<Text style={{fontSize:20}}>{this.props.children}</Text>
           		<Text onPress={()=>Linking.openURL(this.props.user.html_url) }>{this.props.user.html_url}</Text>          	
